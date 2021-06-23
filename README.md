54f1c1f69991a9bff7f434fcea70a939d279da5f
wuyu <191287278@qq.com>
2021-06-23 12:05:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# [Websockify](https://github.com/novnc/websockify)
+
+## Build
+
+``` bash
+go build -d run
+```
+
+## Run
+
+```bash
+./run --address localhost:8888 --target localhost:5900
+```
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"golang.org/x/net/websocket"
+	"io"
+	"log"
+	"net"
+	"net/http"
+)
+
+func main() {
+	address := flag.String("address", ":8888", "Server port")
+	vncAddress := flag.String("target", "localhost:5900", "Vnc Server port")
+	flag.Parse()
+	http.Handle("/websockify", websocket.Handler(func(wsconn *websocket.Conn) {
+		defer wsconn.Close()
+		var d net.Dialer
+		var address = *vncAddress
+		conn, err := d.DialContext(wsconn.Request().Context(), "tcp", address)
+		if err != nil {
+			log.Printf("[%s] [VNC_ERROR] [%v]", address, err)
+			return
+		}
+		defer conn.Close()
+		wsconn.PayloadType = websocket.BinaryFrame
+		go func() {
+			io.Copy(wsconn, conn)
+			wsconn.Close()
+			log.Printf("[%s] [VNC_SESSION_CLOSED]", address)
+		}()
+		io.Copy(conn, wsconn)
+		log.Printf("[%s] [VNC_CLIENT_DISCONNECTED]", address)
+	}))
+	log.Printf("Http listening on %s \n", *address)
+
+	log.Fatal(http.ListenAndServe(*address, nil))
+
+}
