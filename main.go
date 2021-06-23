24119cfd4489b921a4c3d44df7024f03a8445e04
wuyu <191287278@qq.com>
2021-06-23 12:08:44 +0800

    init

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-	address := flag.String("address", ":8888", "Server port")
-	vncAddress := flag.String("target", "localhost:5900", "Target port")
+	httpAddress := flag.String("address", ":8888", "Server port")
+	targetAddress := flag.String("target", "localhost:5900", "Target port")
 	flag.Parse()
 	http.Handle("/websockify", websocket.Handler(func(wsconn *websocket.Conn) {
 		defer wsconn.Close()
 		var d net.Dialer
-		var address = *vncAddress
+		var address = *targetAddress
 		conn, err := d.DialContext(wsconn.Request().Context(), "tcp", address)
 		if err != nil {
 			log.Printf("[%s] [ERROR] [%v]", address, err)
@@ -32,8 +32,8 @@ func main() {
 		io.Copy(conn, wsconn)
 		log.Printf("[%s] [CLIENT_DISCONNECTED]", address)
 	}))
-	log.Printf("Http listening on %s \n", *address)
+	log.Printf("Http listening on %s \n", *httpAddress)
 
-	log.Fatal(http.ListenAndServe(*address, nil))
+	log.Fatal(http.ListenAndServe(*httpAddress, nil))
 
 }
